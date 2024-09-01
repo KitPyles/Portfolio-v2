63f0f66ed9610c3f7c5db01064cd0e4a64b99ea4
Kit Pyles <pyles.kit@gmail.com>
2024-09-01 13:23:51 -0500

    fix: Update TODOs on Education page.

--- app/education/page.js
+++ app/education/page.js
@@ -1,16 +1,24 @@
 
 
-// TODO: Plan & build Education component. Create object array to hold data. Iterate through array to populate cards.
+// TODO: Plan & build Education page. Create object array to hold data. Iterate through array to populate cards.
 
 import { Container, Typography } from "@mui/material"
 
+// TODO: Headstarter. Overview of fellowship. Button links to projects.
+
+const headstarter = {
+  name: "Headstarter Software Engineering Fellowship, Summer 2024",
+  description: "",
+  url: ""
+}
+
 // TODO: LaunchCode. Overview of Web Dev bootcamp. Button link to launchcode.org.
 const launchcode = {
-    name: "LaunchCode",
-    description: "",
-    url: "https://www.launchcode.org"
-  }
-  // TODO: Salem College. Brief overview of the school. Mention of completing 2 BAs in 4 years. Button link to salem.edu.
+  name: "LaunchCode",
+  description: "",
+  url: "https://www.launchcode.org"
+}
+// TODO: Salem College. Brief overview of the school. Mention of completing 2 BAs in 4 years. Button link to salem.edu.
 
 export default function Education() {
   return (
