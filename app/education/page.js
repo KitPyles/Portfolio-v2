907b6f97fe8c213b4915098e1865af01a755e0bb
Kit Pyles <pyles.kit@gmail.com>
2024-09-01 13:20:56 -0500

    test: basic Education page to test nav links

--- app/education/page.js
+++ app/education/page.js
@@ -1,6 +1,9 @@
 
 
 // TODO: Plan & build Education component. Create object array to hold data. Iterate through array to populate cards.
+
+import { Container, Typography } from "@mui/material"
+
 // TODO: LaunchCode. Overview of Web Dev bootcamp. Button link to launchcode.org.
 const launchcode = {
     name: "LaunchCode",
@@ -8,3 +11,16 @@ const launchcode = {
     url: "https://www.launchcode.org"
   }
   // TODO: Salem College. Brief overview of the school. Mention of completing 2 BAs in 4 years. Button link to salem.edu.
+
+export default function Education() {
+  return (
+    <Container>
+      <Typography variant="h2" textAlign={"center"}>
+        Education & Certifications
+      </Typography>
+      <Typography>
+        To be continued...
+      </Typography>
+    </Container>
+  )
+}
