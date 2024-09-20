c62757c43677a5a851bda4385ae2e6d477a8d9fb
Kit Pyles <pyles.kit@gmail.com>
2024-09-19 20:33:35 -0500

    build: Start on Skills backend.

--- app/components/SkillCard.js
+++ app/components/SkillCard.js
@@ -1,3 +1,13 @@
+import theme from "../theme";
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardActions,
+    Typography,
+    Button,
+    Container
+} from "@mui/material"
 
 
 // TODO: Figure out how I want to format the Skill cards on the page.
---
+++ app/skills/skills.js
@@ -0,0 +1,364 @@
+let template = {
+    id: "",
+    name: "",
+    experience: "",
+    projects: ""
+}
+
+let java = {
+    id: "001",
+    name: "Java",
+    experience: "2 years",
+    projects: ""
+}
+
+let javascript = {
+    id: "002",
+    name: "JavaScript | Node.js",
+    experience: "2 years",
+    projects: ""
+}
+
+let nextjs = {
+    id: "003",
+    name: "NextJS",
+    experience: "1 year",
+    projects: ""
+}
+
+let react = {
+    id: "004",
+    name: "React",
+    experience: "1 year",
+    projects: ""
+}
+
+let html = {
+    id: "005",
+    name: "HTML | HTML5",
+    experience: "20 years",
+    projects: ""
+}
+
+let css = {
+    id: "006",
+    name: "Cascading Style Sheets (CSS) | CSS3",
+    experience: "3 years",
+    projects: ""
+}
+
+let typescript = {
+    id: "007",
+    name: "TypeScript",
+    experience: "2 years",
+    projects: ""
+}
+
+let material = {
+    id: "008",
+    name: "Material UI 5",
+    experience: "1 year",
+    projects: ""
+}
+
+let springboot = {
+    id: "009",
+    name: "SpringBoot",
+    experience: "2 years",
+    projects: ""
+}
+
+let thymeleaf = {
+    id: "010",
+    name: "Thymeleaf Templates",
+    experience: "2 years",
+    projects: ""
+}
+
+let sql = {
+    id: "011",
+    name: "MySQL | SQL",
+    experience: "2 years",
+    projects: ""
+}
+
+let firebase = {
+    id: "012",
+    name: "Firebase | Firestore",
+    experience: "1 year",
+    projects: ""
+}
+
+let googlegenai = {
+    id: "013",
+    name: "Google GenerativeAI API | Gemini API",
+    experience: "1 year",
+    projects: ""
+}
+
+let railway = {
+    id: "014",
+    name: "Railway.app Deployments",
+    experience: "2 years",
+    projects: ""
+}
+
+let cicd = {
+    id: "015",
+    name: "Continuous Integration/Continuous Development (CICD)",
+    experience: "2 years",
+    projects: ""
+}
+
+let jira = {
+    id: "016",
+    name: "JIRA",
+    experience: "1 year",
+    projects: ""
+}
+
+let trello = {
+    id: "017",
+    name: "Trello",
+    experience: "3 years",
+    projects: ""
+}
+
+let confluence = {
+    id: "018",
+    name: "Confluence",
+    experience: "1 year",
+    projects: ""
+}
+
+let notion = {
+    id: "019",
+    name: "Notion",
+    experience: "2 years",
+    projects: ""
+}
+
+let clickup = {
+    id: "020",
+    name: "ClickUp (Web & Windows Desktop)",
+    experience: "3 years",
+    projects: ""
+}
+
+let github = {
+    id: "021",
+    name: "GitHub",
+    experience: "5 years",
+    projects: ""
+}
+
+let msOutlook = {
+    id: "022",
+    name: "Microsoft Outlook",
+    experience: "20+ years",
+    projects: ""
+}
+
+let msExcel = {
+    id: "023",
+    name: "Microsoft Excel",
+    experience: "20+ years",
+    projects: ""
+}
+
+let msWord = {
+    id: "024",
+    name: "Microsoft Word",
+    experience: "20+ years",
+    projects: ""
+}
+
+let msAccess = {
+    id: "025",
+    name: "Microsoft Access",
+    experience: "20+ years",
+    projects: ""
+}
+
+let msPowerPoint = {
+    id: "026",
+    name: "Microsoft PowerPoint",
+    experience: "20+ years",
+    projects: ""
+}
+
+let msPublisher = {
+    id: "027",
+    name: "Microsoft Publisher",
+    experience: "20+ years",
+    projects: ""
+}
+
+let googleDocs = {
+    id: "028",
+    name: "Google Docs",
+    experience: "6 years",
+    projects: ""
+}
+
+let googleSheets = {
+    id: "029",
+    name: "Google Sheets",
+    experience: "6 years",
+    projects: ""
+}
+
+let googleSlides = {
+    id: "030",
+    name: "Google Slides",
+    experience: "4 years",
+    projects: ""
+}
+
+let googleForms = {
+    id: "031",
+    name: "Google Forms",
+    experience: "4 years",
+    projects: ""
+}
+
+let lighthouse = {
+    id: "032",
+    name: "Google Lighthouse",
+    experience: "2 years",
+    projects: ""
+}
+
+let wave = {
+    id: "033",
+    name: "WAVE Web Accessibility Extension",
+    experience: "2 years",
+    projects: ""
+}
+
+let siteImprove = {
+    id: "034",
+    name: "SiteImprove Accessibility Checker",
+    experience: "2 years",
+    projects: ""
+}
+
+let zephyr = {
+    id: "035",
+    name: "Zephyr Scale",
+    experience: "1 year",
+    projects: ""
+}
+
+let xray = {
+    id: "036",
+    name: "XRAY in JIRA",
+    experience: "1 year",
+    projects: ""
+}
+
+let selenium = {
+    id: "037",
+    name: "Selenium IDE",
+    experience: "1 year",
+    projects: ""
+}
+
+let postman = {
+    id: "038",
+    name: "Postman (API Testing)",
+    experience: "2 years",
+    projects: ""
+}
+
+let jmeter = {
+    id: "039",
+    name: "JMeter",
+    experience: "1 year",
+    projects: ""
+}
+
+let jasmine = {
+    id: "040",
+    name: "Jasmine",
+    experience: "2 years",
+    projects: ""
+}
+
+let debug = {
+    id: "041",
+    name: "Debugging",
+    experience: "3 years",
+    projects: ""
+}
+
+let tdd = {
+    id: "042",
+    name: "Test-Driven Development",
+    experience: "2 years",
+    projects: ""
+}
+
+let agile = {
+    id: "043",
+    name: "Agile Methodologies",
+    experience: "2 years",
+    projects: ""
+}
+
+let scrum = {
+    id: "044",
+    name: "Scrum Methodologies",
+    experience: "2 years",
+    projects: ""
+}
+
+let sdlc = {
+    id: "045",
+    name: "Software Development Life Cycle (SDLC)",
+    experience: "2 years",
+    projects: ""
+}
+
+let pm = {
+    id: "046",
+    name: "Project Management",
+    experience: "5 years",
+    projects: ""
+}
+
+let wcag = {
+    id: "047",
+    name: "Web Content Accessibility Guidelines (WCAG)",
+    experience: "2 years",
+    projects: ""
+}
+
+let oop = {
+    id: "048",
+    name: "Object-Oriented Programming (OOP)",
+    experience: "2 years",
+    projects: ""
+}
+
+let prompt = {
+    id: "049",
+    name: "Effective Prompt Engineering (Google Search, ChatGPT, Google Gemini, Perplexity)",
+    experience: "20+ years",
+    projects: ""
+}
+
+let written = {
+    id: "050",
+    name: "Written Communication",
+    experience: "20+ years",
+    projects: ""
+}
+
+let oral = {
+    id: "051",
+    name: "Oral Communication",
+    experience: "20+ years",
+    projects: ""
+}
+
