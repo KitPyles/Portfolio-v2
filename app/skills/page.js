08b3926b05111d39a8c62d9fb878179b0b6a848a
Kit Pyles <pyles.kit@gmail.com>
2024-09-26 20:18:14 -0500

    fix: Troubleshoot flex-wrap error.

--- app/skills/page.js
+++ app/skills/page.js
@@ -1,5 +1,3 @@
-
-
 // TODO: Plan & build Skills component.
 // TODO: List skills in categories.
 // TODO: Use icons for tech stack skills?
@@ -9,28 +7,61 @@ import { frontend, backend, tools, testing, methods } from "../skills/skills";
 import SkillCard from "../components/SkillCard";
 
 export default function Skills() {
-    return(
-        <Container>
-            <Typography variant="h1">Contact Page</Typography>
-            <Typography>work in progress</Typography>
-            <hr/>
-            <Typography>
-                <strong>Front-End Skills:</strong>
-            </Typography>
-            <Box
-                // display={"flex"}
-                // flexDirection={"row"}
-                // flexWrap={"wrap"}
-                // maxHeight={"100vh"}
-                // maxWidth={"100vw"}
-            >
-                {frontend.map((skill) =>
-                    <SkillCard
-                        key={skill.id}
-                        skill={skill}
-                    />
-                )}
-            </Box>
-        </Container>
-    )
+  return (
+    <Container>
+      <Typography variant="h1">Contact Page</Typography>
+      <Typography>work in progress</Typography>
+      <hr />
+      <Container>
+        <Typography>
+          <strong>Front-End Skills:</strong>
+        </Typography>
+        <Box display={"flex"} flexWrap={"wrap"}>
+          {frontend.map((skill) => (
+            <SkillCard key={skill.id} skill={skill} />
+          ))}
+        </Box>
+      </Container>
+      <Container>
+        <Typography>
+          <strong>Back-End Skills:</strong>
+        </Typography>
+        <Box display={"flex"} flexWrap={"wrap"}>
+          {backend.map((skill) => (
+            <SkillCard key={skill.id} skill={skill} />
+          ))}
+        </Box>
+      </Container>
+      <Container>
+        <Typography>
+          <strong>Technical Tools:</strong>
+        </Typography>
+        <Box display={"flex"} flexWrap={"wrap"}>
+          {tools.map((skill) => (
+            <SkillCard key={skill.id} skill={skill} />
+          ))}
+        </Box>
+      </Container>
+      <Container>
+        <Typography>
+          <strong>Testing/QA Skills:</strong>
+        </Typography>
+        <Box display={"flex"} flexWrap={"wrap"}>
+          {testing.map((skill) => (
+            <SkillCard key={skill.id} skill={skill} />
+          ))}
+        </Box>
+      </Container>
+      <Container>
+        <Typography>
+          <strong>Methodologies & Miscellaneous Skills:</strong>
+        </Typography>
+        <Box display={"flex"} flexWrap={"wrap"}>
+          {methods.map((skill) => (
+            <SkillCard key={skill.id} skill={skill} />
+          ))}
+        </Box>
+      </Container>
+    </Container>
+  );
 }
